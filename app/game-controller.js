5778cc928761a6dfd6f47bf3c51724753c0424b6
Jake Overall <joverall22@gmail.com>
2016-02-16 11:01:14 -0700

    Move Lancelot with Keys

--- app/game-controller.js
+++ app/game-controller.js
@@ -1,7 +1,46 @@
-app.controller('GameController', function($scope){
-   $scope.y = 0;
-    
-   $scope.moveDown = function(){
-       $scope.y++;
-   } 
+app.controller('GameController', function ($scope) {
+    var maxBounds = 96;
+    var minBounds = 0;
+    var moveSpeedMod = 3;
+    var moveSpeed = 1;
+     
+    $scope.y = 0;
+    $scope.x = 0;
+	
+    $scope.move = function(e){
+        if(e.keyCode == 39){
+            $scope.moveRight();
+        } else if (e.keyCode == 40){
+            $scope.moveDown();
+        } else if (e.keyCode == 38){
+            $scope.moveUp();
+        } else if(e.keyCode == 37){
+            $scope.moveLeft();
+        }
+    }
+     
+    $scope.moveDown = function () {
+        if ($scope.y < maxBounds) {
+            $scope.y += moveSpeed * moveSpeedMod;
+        }
+    }
+
+    $scope.moveRight = function () {
+        if ($scope.x < maxBounds) {
+            $scope.x += moveSpeed * moveSpeedMod;
+        }
+    }
+
+    $scope.moveUp = function () {
+        if ($scope.y > minBounds) {
+            $scope.y -= moveSpeed * moveSpeedMod;
+        }
+    }
+
+    $scope.moveLeft = function () {
+        if ($scope.x > minBounds) {
+            $scope.x -= moveSpeed * moveSpeedMod;
+        }
+    }
+
 });
